ebdeb0949b5cfe14b471d73790ea854575f5a980
Ilan Usman <ilanusman03@gmail.com>
2025-03-08 22:02:12 +0530

    svg

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -186,7 +186,7 @@ const Nav = () => {
       <div className="navbar">
         <div className="logo">
           <Link href="/">
-            <img className="logoicon" src="/logos/starshape.png" alt="Logo" />
+            <img className="logoicon" src="/logos/starshape.svg" />
           </Link>
         </div>
 
